fef002c94887ee3107e0e4ccb2ca0a3be252acb8
Max Mays <cheeser12@ezen.com>
2013-08-13 21:06:18 +0000

    Added carousel with images

--- web.js
+++ web.js
@@ -1,11 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-    var buffer = new Buffer(fs.readFileSync('index.html'));
-    response.send(buffer.toString());
-});
+app.use(express.static(__dirname + '/'));
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
